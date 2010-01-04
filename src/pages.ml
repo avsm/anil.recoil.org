9ab97eb59e7240b5ca76d66087fe73c3c7587c96
Anil Madhavapeddy <anil@recoil.org>
2010-01-04 10:32:25 +0000

    update to serve the index page

--- src/pages.ml
+++ src/pages.ml
@@ -1,10 +1,39 @@
 open Printf
+open Htmlgen
+module ST = Static_templates
+
+(* get template body *)
+let get n = match ST.t n with None -> failwith (n ^ " not found") | Some x -> x
+(* substitute markdown from template *)
+let md n = Markdown_html.t (Markdown.parse_text (get n))
+
+let header = get "header.inc"
+let footer = get "footer.inc"
+
+(* substitute keys in hashtbl h in string s *)
+let subst s h =
+  let re frm tos s =
+    let rex = Pcre.regexp frm in
+    let itempl = Pcre.subst tos in
+    Pcre.replace ~rex ~itempl s in
+  Hashtbl.fold re h s
+
+(* new hash with title key *)
+let keys ?extra title =
+  let h = Hashtbl.create 1 in
+  Hashtbl.add h "@@TITLE@@" title;
+  (match extra with 
+  |None -> ()
+  |Some l -> List.iter (fun (k,v) -> Hashtbl.add h k v) l);
+  h
+
+let page p h = subst (header ^ p ^ footer) h
 
 module Index = struct
 
+  let h = keys ~extra:[ "@@CONTENT@@", md "intro.md" ] "index"
+ 
   let t =
-    match Static_templates.t "tmpl.inc" with 
-    | Some b ->
-       b
+    page (get "index.inc") h
 
 end
